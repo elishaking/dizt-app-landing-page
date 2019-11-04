49c64a09a805d1fb50a085c09669e89b19606681
King Elisha <ek.chibueze@gmail.com>
2019-11-04 03:36:54 +0100

    landing component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import Landing from './components/Landing';
 
 function App() {
   return (
     <div className="App">
-      
+      <Landing />
     </div>
   );
 }
