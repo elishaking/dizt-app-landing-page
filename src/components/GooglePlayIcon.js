557e1382c5c60cf13ef500c1e11e88d16ad1de7c
King Elisha <ek.chibueze@gmail.com>
2019-11-04 03:48:25 +0100

    markup and assets

---
+++ src/components/GooglePlayIcon.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+export default function GooglePlayIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="271" height="80" viewBox="0 0 271 80">
+      <defs>
+        <linearGradient id="linear-gradient" x1="0.915" y1="6.614" x2="-0.383" y2="5.945" gradientUnits="objectBoundingBox">
+          <stop offset="0" stopColor="#00a0ff" />
+          <stop offset="0.007" stopColor="#00a1ff" />
+          <stop offset="0.26" stopColor="#00beff" />
+          <stop offset="0.512" stopColor="#00d2ff" />
+          <stop offset="0.76" stopColor="#00dfff" />
+          <stop offset="1" stopColor="#00e3ff" />
+        </linearGradient>
+        <linearGradient id="linear-gradient-2" x1="1.076" y1="17.098" x2="-1.305" y2="17.098" gradientUnits="objectBoundingBox">
+          <stop offset="0" stopColor="#ffe000" />
+          <stop offset="0.409" stopColor="#ffbd00" />
+          <stop offset="0.775" stopColor="orange" />
+          <stop offset="1" stopColor="#ff9c00" />
+        </linearGradient>
+        <linearGradient id="linear-gradient-3" x1="0.862" y1="10.863" x2="-0.501" y2="9.093" gradientUnits="objectBoundingBox">
+          <stop offset="0" stopColor="#ff3a44" />
+          <stop offset="1" stopColor="#c31162" />
+        </linearGradient>
+        <linearGradient id="linear-gradient-4" x1="-0.188" y1="13.583" x2="0.421" y2="12.793" gradientUnits="objectBoundingBox">
+          <stop offset="0" stopColor="#32a071" />
+          <stop offset="0.068" stopColor="#2da771" />
+          <stop offset="0.476" stopColor="#15cf74" />
+          <stop offset="0.801" stopColor="#06e775" />
+          <stop offset="1" stopColor="#00f076" />
+        </linearGradient>
+      </defs>
+      <g id="Group_6" data-name="Group 6" transform="translate(-10 -10)">
+        <rect id="Rectangle_8" data-name="Rectangle 8" width="271" height="80" rx="5" transform="translate(10 10)" fill="#005e8d" />
+        <g id="Group_8" data-name="Group 8" transform="translate(93.074 23.794)">
+          <path id="Path_8" data-name="Path 8" d="M63.538,23.65a5.441,5.441,0,0,1-1.5,4.024,5.842,5.842,0,0,1-4.428,1.784,6.209,6.209,0,0,1-6.262-6.3,6.209,6.209,0,0,1,6.262-6.3,6.24,6.24,0,0,1,2.475.5,4.959,4.959,0,0,1,1.884,1.348l-1.059,1.059a4.077,4.077,0,0,0-3.3-1.428,4.661,4.661,0,0,0-4.681,4.813,4.741,4.741,0,0,0,8.048,3.469,3.8,3.8,0,0,0,1.01-2.443H57.614V22.742h5.842A5.137,5.137,0,0,1,63.538,23.65Z" transform="translate(-51.352 -16.866)" fill="#fff" stroke="#fff" strokeMiterlimit="10" strokeWidth="0.2" />
+          <path id="Path_9" data-name="Path 9" d="M65.562,18.481h-5.49V22.3h4.95V23.75h-4.95v3.823h5.49v1.481H58.525V17h7.037Z" transform="translate(-44.115 -16.731)" fill="#fff" stroke="#fff" strokeMiterlimit="10" strokeWidth="0.2" />
+          <path id="Path_10" data-name="Path 10" d="M67.748,29.054H66.2V18.481H62.832V17h8.283v1.481H67.748Z" transform="translate(-39.769 -16.731)" fill="#fff" stroke="#fff" strokeMiterlimit="10" strokeWidth="0.2" />
+          <path id="Path_11" data-name="Path 11" d="M69.938,29.054V17h1.547V29.054Z" transform="translate(-32.6 -16.731)" fill="#fff" stroke="#fff" strokeMiterlimit="10" strokeWidth="0.2" />
+          <path id="Path_12" data-name="Path 12" d="M76.6,29.054H75.048V18.481H71.681V17h8.283v1.481H76.6Z" transform="translate(-30.841 -16.731)" fill="#fff" stroke="#fff" strokeMiterlimit="10" strokeWidth="0.2" />
+          <path id="Path_13" data-name="Path 13" d="M88.939,27.632a6.259,6.259,0,0,1-8.837,0,6.164,6.164,0,0,1-1.776-4.47,6.164,6.164,0,0,1,1.776-4.47,6.24,6.24,0,0,1,8.829.008,6.162,6.162,0,0,1,1.784,4.462A6.172,6.172,0,0,1,88.939,27.632Zm-7.694-1.011a4.637,4.637,0,0,0,6.549,0,4.718,4.718,0,0,0,1.34-3.459,4.718,4.718,0,0,0-1.34-3.459,4.637,4.637,0,0,0-6.549,0,4.724,4.724,0,0,0-1.338,3.459A4.724,4.724,0,0,0,81.245,26.621Z" transform="translate(-24.136 -16.866)" fill="#fff" stroke="#fff" strokeMiterlimit="10" strokeWidth="0.2" />
+          <path id="Path_14" data-name="Path 14" d="M85.575,29.054V17h1.886l5.858,9.376h.066l-.066-2.322V17h1.549V29.054H93.253l-6.129-9.832h-.066l.066,2.324v7.507Z" transform="translate(-16.823 -16.731)" fill="#fff" stroke="#fff" strokeMiterlimit="10" strokeWidth="0.2" />
+        </g>
+        <path id="Path_15" data-name="Path 15" d="M105.473,36.5a8.544,8.544,0,1,0,8.576,8.544A8.421,8.421,0,0,0,105.473,36.5Zm0,13.721a4.943,4.943,0,0,1-4.824-5.177,4.835,4.835,0,1,1,9.645,0A4.941,4.941,0,0,1,105.473,50.22ZM86.763,36.5a8.544,8.544,0,1,0,8.576,8.544A8.422,8.422,0,0,0,86.763,36.5Zm0,13.721a4.943,4.943,0,0,1-4.823-5.177,4.835,4.835,0,1,1,9.645,0A4.943,4.943,0,0,1,86.763,50.22Zm-22.257-11.1v3.624h8.673a7.565,7.565,0,0,1-1.973,4.562,8.881,8.881,0,0,1-6.7,2.654,9.645,9.645,0,0,1,0-19.288,9.241,9.241,0,0,1,6.537,2.59L73.6,30.705a12.647,12.647,0,0,0-9.095-3.658,13.272,13.272,0,1,0,0,26.54,12.126,12.126,0,0,0,9.255-3.723,11.977,11.977,0,0,0,3.14-8.48,11.71,11.71,0,0,0-.195-2.264Zm91.022,2.815c-.711-1.909-2.881-5.436-7.315-5.436-4.4,0-8.058,3.461-8.058,8.544a8.361,8.361,0,0,0,8.48,8.544,8.5,8.5,0,0,0,7.12-3.787l-2.913-1.943a4.881,4.881,0,0,1-4.207,2.363,4.349,4.349,0,0,1-4.144-2.588l11.425-4.727Zm-11.652,2.849a4.688,4.688,0,0,1,4.466-4.984,3.305,3.305,0,0,1,3.172,1.812Zm-9.287,8.285h3.753V27.953h-3.753Zm-6.151-14.661h-.129a5.922,5.922,0,0,0-4.5-1.909,8.552,8.552,0,0,0,0,17.088,5.821,5.821,0,0,0,4.5-1.943h.129v1.231c0,3.269-1.746,5.016-4.562,5.016a4.729,4.729,0,0,1-4.305-3.044l-3.269,1.36a8.142,8.142,0,0,0,7.574,5.049c4.4,0,8.124-2.59,8.124-8.9V37.015h-3.562Zm-4.3,11.813a4.9,4.9,0,0,1-4.757-5.145,4.942,4.942,0,0,1,4.757-5.211c2.557,0,4.562,2.2,4.562,5.211C128.7,48.053,126.691,50.22,124.134,50.22Zm48.98-22.265h-8.982V53.069h3.747V43.555h5.235c4.155,0,8.239-3.009,8.239-7.8S177.269,27.955,173.114,27.955Zm.1,12.1h-5.332V31.451h5.332a4.305,4.305,0,1,1,0,8.608Zm23.167-3.606c-2.714,0-5.527,1.2-6.69,3.843l3.329,1.39a3.551,3.551,0,0,1,3.425-1.842c1.939,0,3.909,1.163,3.942,3.232v.257a8.292,8.292,0,0,0-3.909-.968c-3.586,0-7.238,1.971-7.238,5.653,0,3.361,2.941,5.527,6.236,5.527a5.289,5.289,0,0,0,4.783-2.457h.129v1.939H204V43.4C204,38.94,200.675,36.453,196.378,36.453Zm-.452,13.763c-1.229,0-2.941-.613-2.941-2.132,0-1.939,2.132-2.682,3.974-2.682a6.688,6.688,0,0,1,3.425.84A4.544,4.544,0,0,1,195.926,50.216ZM217.184,37l-4.3,10.889h-.129L208.3,37h-4.038l6.688,15.218-3.813,8.466h3.909L221.353,37ZM183.42,53.069h3.749V27.955H183.42Z" transform="translate(41.409 17.2)" fill="#fff" />
+        <g id="google-play" transform="translate(30.035 24.345)">
+          <path id="Path_16" data-name="Path 16" d="M20.9,17.612a4.032,4.032,0,0,0-.928,2.823v44.43a4.028,4.028,0,0,0,.928,2.823l.149.145L45.939,42.943v-.587L21.05,17.465Z" transform="translate(-19.973 -16.813)" fill="url(#linear-gradient)" />
+          <path id="Path_17" data-name="Path 17" d="M41.195,42.911l-8.3-8.3v-.587l8.3-8.3.187.108,9.83,5.585c2.807,1.6,2.807,4.2,0,5.8L41.384,42.8Z" transform="translate(-6.932 -8.48)" fill="url(#linear-gradient-2)" />
+          <path id="Path_18" data-name="Path 18" d="M53.958,38.487,45.473,30,20.435,55.039c.926.98,2.455,1.1,4.177.123L53.958,38.487" transform="translate(-19.507 -4.165)" fill="url(#linear-gradient-3)" />
+          <path id="Path_19" data-name="Path 19" d="M53.958,34.491,24.612,17.817c-1.722-.978-3.25-.858-4.177.123L45.473,42.977Z" transform="translate(-19.507 -17.14)" fill="url(#linear-gradient-4)" />
+          <g id="Group_9" data-name="Group 9" transform="translate(0 34.135)">
+            <path id="Path_20" data-name="Path 20" d="M53.772,34.132,24.612,50.7c-1.631.928-3.09.866-4.026.022h0l-.151.149h0l.149.147,0,0c.936.844,2.395.906,4.026-.022L53.958,34.319Z" transform="translate(-19.507 -34.132)" opacity="0.2" />
+            <path id="Path_21" data-name="Path 21" d="M20.9,43.736a4.028,4.028,0,0,1-.928-2.823v.3a4.023,4.023,0,0,0,.928,2.821l.151-.149Z" transform="translate(-19.973 -27.29)" opacity="0.12" />
+          </g>
+          <path id="Path_22" data-name="Path 22" d="M47.046,32.609,37.029,38.3l.187.187,9.83-5.585A3.526,3.526,0,0,0,49.151,30h0A3.738,3.738,0,0,1,47.046,32.609Z" transform="translate(-2.764 -4.165)" opacity="0.12" />
+          <path id="Path_23" data-name="Path 23" d="M25.078,18.11,64.254,40.369a3.738,3.738,0,0,1,2.105,2.608h0a3.521,3.521,0,0,0-2.105-2.9L25.078,17.817c-2.809-1.6-5.1-.269-5.1,2.945v.3C19.973,17.841,22.269,16.515,25.078,18.11Z" transform="translate(-19.973 -17.14)" fill="#fff" opacity="0.25" />
+        </g>
+      </g>
+    </svg>
+  )
+}
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,65 @@
 import React, { Component } from 'react';
+import GooglePlayIcon from './GooglePlayIcon';
 
 export default class Landing extends Component {
   render() {
     return (
       <div id="landing">
+        <header>
+          <nav>
+            <h1>
+              <svg xmlns="http://www.w3.org/2000/svg" width="46.908" height="46.908" viewBox="0 0 46.908 46.908">
+                <g id="logo" transform="translate(-161.5 -161.5)">
+                  <path id="Path_3" data-name="Path 3" d="M192.337,174.665c-5.683,6.618-15.714,8.769-19.777,19.631a56.308,56.308,0,0,0-3.21,14.111h-5.529a2.328,2.328,0,0,1-2.321-2.321V163.821a2.328,2.328,0,0,1,2.321-2.321h31.1A13.326,13.326,0,0,1,192.337,174.665Z" fill="#347fa2" />
+                  <path id="Path_4" data-name="Path 4" d="M608.453,163.821v42.265a2.328,2.328,0,0,1-2.321,2.321H594.494c-2.121-4.989-4.216-11.806-2.583-17.057,2.844-9.147,9.081-15.578,8.953-20.885-.1-4.005-2.687-7.579-3.852-8.965h9.12A2.328,2.328,0,0,1,608.453,163.821Z" transform="translate(-400.045)" fill="#62c5f2" />
+                </g>
+              </svg>
 
+              Dizt
+            </h1>
+
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="#features">Features</a></li>
+              <li><a href="#screenshots">Screenshots</a></li>
+              <li><a href="#blog">Blog</a></li>
+            </ul>
+          </nav>
+        </header>
+
+        <div className="content">
+          <div className="left">
+            <h1>Know the distance and time to your destination</h1>
+
+            <p>Dizt is a simple and intuitive mobile app that enables you to determine the road distance between two locations and the exact travel time using your car, the bus, your bicycle or by walking. You also get to see the transport fare if you are using a bus along some routes</p>
+
+            <div id="g-play">
+              <a href="https://play.google.com/store/apps/details?id=com.king.apps.dizt" rel="noopener noreferrer" target="_blank">
+                <GooglePlayIcon />
+              </a>
+            </div>
+
+            <div>
+              <a href="https://facebook.com/skyblazar" rel="noopener noreferrer" target="_blank">
+                <svg id="Group_63" data-name="Group 63" xmlns="http://www.w3.org/2000/svg" width="45.026" height="45.026" viewBox="0 0 45.026 45.026">
+                  <g id="facebook" transform="translate(16.279 10.391)">
+                    <path id="f_1_" d="M45.462,24.245V13.186h3.71l.557-4.311H45.462V6.124c0-1.248.345-2.1,2.136-2.1H49.88V.169A30.927,30.927,0,0,0,46.555,0c-3.291,0-5.544,2.009-5.544,5.7V8.875H37.29v4.311h3.722V24.245Z" transform="translate(-37.29)" fill="#005e8d" />
+                  </g>
+                  <g id="Ellipse_38" data-name="Ellipse 38" transform="translate(0 0)" fill="none" stroke="#005e8d" stroke-width="3">
+                    <circle cx="22.513" cy="22.513" r="22.513" stroke="none" />
+                    <circle cx="22.513" cy="22.513" r="21.013" fill="none" />
+                  </g>
+                </svg>
+              </a>
+            </div>
+          </div>
+
+          <div className="right">
+            <div>
+              <img src="img/phone.png" alt="Phone" />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
